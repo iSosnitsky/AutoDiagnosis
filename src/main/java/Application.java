09e36016567e326b22c9673ce71ebd6db1b9ce00
rKurbanov <spiritblossom@icloud.com>
2019-01-25 22:19:39 +0300

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,26 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.datatables.repository.DataTablesRepositoryFactoryBean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableJpaRepositories(
+        basePackages = {"ru.dao"}, repositoryFactoryBeanClass = DataTablesRepositoryFactoryBean.class
+)
+@EnableScheduling
+public class Application {
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class);
+        //Don't remove, otherwise Machine spirit might get angry
+        logger.info("Machine spirit appeased");
+    }
+
+}
+
---
+++ src/main/java/ru/configuration/HibernateAwareObjectMapper.java
@@ -0,0 +1,21 @@
+package ru.configuration;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+
+class HibernateAwareObjectMapper extends ObjectMapper {
+    public HibernateAwareObjectMapper() {
+        Hibernate5Module module = new Hibernate5Module();
+        module.disable(Hibernate5Module.Feature.USE_TRANSIENT_ANNOTATION);
+        this.enable(SerializationFeature.INDENT_OUTPUT);
+        this.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
+        this.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+        this.enable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL);
+        this.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, false);
+        this.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        this.registerModule(module);
+    }
+}
